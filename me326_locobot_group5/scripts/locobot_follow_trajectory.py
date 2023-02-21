4762059449b6e7fcb8f3758738f4e19cf5249819
Devin Ardeshna <devin@ardeshna.com>
2023-02-21 00:38:28 -0800

    trajectory following changes from lab testing and acceleration limit

--- me326_locobot_group5/scripts/locobot_follow_trajectory.py
+++ me326_locobot_group5/scripts/locobot_follow_trajectory.py
@@ -35,6 +35,14 @@ class LocobotFollowTrajectory(object):
     
     def __init__(self):
 
+        self.robot_name = rospy.get_param('/robot_name')
+        self.robot_type = rospy.get_param('/robot_type')
+        self.frame_offset = {
+            'x' : rospy.get_param('/frame_offset/x'),
+            'y' : rospy.get_param('/frame_offset/y'),
+            'theta' : rospy.get_param('/frame_offset/theta')
+        }
+
         self.mobile_base_vel_publisher = rospy.Publisher("/locobot/mobile_base/commands/velocity", Twist, queue_size=1) #this is the topic we will publish to in order to move the base
 
         self.target_pose = Pose()
@@ -48,8 +56,20 @@ class LocobotFollowTrajectory(object):
             [5, -1],
         ]) / 2
 
+        # self.path = np.array([
+        #     [-0.4, 1.8],
+        #     [0, 1.8],
+        #     [0.5, 1.9],
+        #     [0.6, 1.9]
+        # ])
+
+        if self.robot_type == "sim":
+            self.frame_id = "locobot/odom"
+        elif self.robot_type == "physical":
+            self.frame_id = "map"
+
         self.path_msg = Path(
-            header=Header(frame_id='/locobot/odom'),
+            header=Header(frame_id=self.frame_id),
             poses=[PoseStamped(pose=Pose(position=Point(x=i[0], y=i[1]))) for i in self.path]
         )
         self.path_visual = rospy.Publisher('/locobot/mobile_base/path_visual', Path, queue_size=1)
@@ -57,40 +77,57 @@ class LocobotFollowTrajectory(object):
         # generate a basic spline trajectory for demonstration
 
         # control limits
-        self.v_max = 0.5 # m/s
-        self.w_max = np.pi/2 # rad/s
+        self.v_max = 0.3 # m/s
+        self.w_max = np.pi/4 # rad/s
 
         # traj gen limits
-        self.v_lim = 0.3 # m/s
-        self.w_lim = np.pi/4 # rad/s
+        self.v_lim = 0.1 # m/s
+        self.a_lim = 0.03 # m/s^2
+        self.w_lim = np.pi/8 # rad/s
 
         alpha = 0.5 # smoothing parameter
 
-        traj, traj_ts, = compute_smoothed_traj(self.path, self.v_max, alpha, CTRL_DT)
-        traj_ts, v_goal, w_goal, traj = modify_traj_with_limits(traj, traj_ts, self.v_lim, self.w_lim, CTRL_DT)
+        traj, traj_ts, = compute_smoothed_traj(self.path, self.v_lim, alpha, CTRL_DT)
+        traj_ts, v_goal, w_goal, traj = modify_traj_with_limits(traj, traj_ts, self.v_lim, self.a_lim, self.w_lim, CTRL_DT)
 
         self.traj = scipy.interpolate.interp1d(traj_ts, traj, axis=0, bounds_error=False, fill_value=(traj[0], traj[-1]))
         self.v_goal = scipy.interpolate.interp1d(traj_ts, v_goal, bounds_error=False, fill_value=0)
         self.w_goal = scipy.interpolate.interp1d(traj_ts, w_goal, bounds_error=False, fill_value=0)
+        self.t_final = traj_ts[-1]
         self.x_final = traj[-1, 0:3]
         self.theta_final = traj[-1, 2] + np.pi/2
+
+        self.traj_msg = Path(
+            header=Header(frame_id=self.frame_id),
+            poses=[PoseStamped(pose=Pose(position=Point(x=(traj_pt := self.traj(t))[0], y=traj_pt[1]))) for t in np.linspace(0, self.t_final, 100)]
+        )
+        self.traj_visual = rospy.Publisher('/locobot/mobile_base/traj_visual', Path, queue_size=1)
 
         # set up controllers
         self.ramsete = Ramsete(b=2.0, zeta=0.8)
         self.pose_controller = PoseController(k1=0.4, k2=0.8, k3=0.8)
-        self.heading_kP = 2
-        self.dist_thresh = 0.5
+        self.heading_kP = 0.5
+        self.dist_thresh = 0.2
         self.heading_thresh = 0.05
 
         # initialize measurements
         self.current_pose = Pose()
         self.current_euler = np.zeros(3)
+        self.current_vel = Twist()
+
+        self.vel_error = 0
+        self.w_error = 0
+
+        self.controller = "ramsete"
+
+        self.last_v = 0
+        self.last_w = 0
 
 
     def pub_target_point_marker(self):
         #this is putting the marker in the world frame (http://wiki.ros.org/rviz/DisplayTypes/Marker#Points_.28POINTS.3D8.29)
         marker = Marker()
-        marker.header.frame_id = "locobot/odom" #this will be the world frame for the real robot
+        marker.header.frame_id = self.frame_id #this will be the world frame for the real robot
         marker.header.stamp = rospy.Time.now()
         marker.id = 0
         marker.type = Marker.ARROW
@@ -117,15 +154,35 @@ class LocobotFollowTrajectory(object):
         # Publish the marker
         self.target_pose_visual.publish(marker)
 
-    def mobile_base_callback(self, data):
+    def locobot_odom_callback(self, data):
         
-        self.current_pose = data.pose.pose
+        if self.robot_type == "sim":
+
+            self.current_pose = data.pose.pose
+
+            self.current_euler = R.from_quat([
+                data.pose.pose.orientation.x,
+                data.pose.pose.orientation.y,
+                data.pose.pose.orientation.z,
+                data.pose.pose.orientation.w,
+            ]).as_euler('ZYX')
+
+        self.current_vel = data.twist.twist
+
+    def mocap_callback(self, data):
+        
+        self.current_pose = data.pose
+
         self.current_euler = R.from_quat([
-            data.pose.pose.orientation.x,
-            data.pose.pose.orientation.y,
-            data.pose.pose.orientation.z,
-            data.pose.pose.orientation.w,
+            data.pose.orientation.x,
+            data.pose.orientation.y,
+            data.pose.orientation.z,
+            data.pose.orientation.w,
         ]).as_euler('ZYX')
+        
+        self.current_euler[0] += self.frame_offset['theta']
+        self.current_pose.position.x += self.frame_offset['x'] * np.cos(self.current_euler[0])
+        self.current_pose.position.y += self.frame_offset['y'] * np.sin(self.current_euler[0])
 
     def compute_control(self):
 
@@ -148,37 +205,79 @@ class LocobotFollowTrajectory(object):
 
         self.pub_target_point_marker()
 
-        if np.linalg.norm(self.x_final[0:2]-x[0:2]) >= self.dist_thresh:
+        if self.controller == "ramsete":
+
             v, w = self.ramsete.calculate(x_goal, v_goal, w_goal, x)
-            print('ramsete')
+            print('ramsete controller')
             print('x: ', x)
             print('x_goal: ', x_goal)
             print('tracking_error: ', np.linalg.norm(x_goal[0:2]-x[0:2]))
-        elif np.linalg.norm(self.x_final[0:2]-x[0:2]) >= self.heading_thresh:
+
+            if np.linalg.norm(self.x_final[0:2]-x[0:2]) < self.dist_thresh or t > self.t_final:
+                self.controller = "pose"
+            
+        elif self.controller == "pose":
+
             v, w = self.pose_controller.calculate(self.x_final, x)
             print('pose controller')
             print('x: ', x)
             print('x_goal: ', self.x_final)
             print('pose_error: ', np.linalg.norm(self.x_final[0:2]-x[0:2]))
-        else:
+
+            if np.linalg.norm(self.x_final[0:2]-x[0:2]) < self.heading_thresh:
+                self.controller = "heading"
+            
+        elif self.controller == "heading":
+
             v, w = (0, self.heading_kP * ((self.theta_final - x[2] + np.pi) % (2*np.pi) - np.pi))
-            print('finished')
+            print('heading controller')
             print('current heading: ', x[2])
             print('goal heading: ', self.theta_final)
             print('heading error: ', self.theta_final-x[2])
+            print('pose_error: ', np.linalg.norm(self.x_final[0:2]-x[0:2]))
 
+        # print('v: ', self.current_vel.linear.x)
+        # print('w: ', self.current_vel.angular.z)
+
+        print('v_goal: ', v_goal)
+        print('w_goal: ', w_goal)
+
+        print('v_cmd: ', v)
+        print('w_cmd: ', w)
+
+        # alpha = 0.8
+
+        # self.vel_error = alpha * self.vel_error + (1-alpha)*(v-self.current_vel.linear.x)
+        # self.w_error = alpha * self.w_error + (1-alpha)*(w-self.current_vel.angular.z)
+
+        # print('vel_error: ', self.vel_error)
+        # print('w_error: ', self.w_error)
         
-
         control_msg = Twist()
         control_msg.linear.x = np.clip(v, -self.v_max, self.v_max)
         control_msg.angular.z = np.clip(w, -self.w_max, self.w_max)
+
+        # # scale and delay control commands to mimic robot dynamics
+
+        # control_msg.linear.x *= 0.8
+        # control_msg.angular.z *= 0.8
+
+        # control_msg.linear.x = 0.99 * self.last_v + (1-0.99) * control_msg.linear.x
+        # control_msg.angular.z = 0.5 * self.last_w + (1-0.5) * control_msg.angular.z
+
+        # self.last_v = control_msg.linear.x
+        # self.last_w = control_msg.angular.z
+        
+        # publish control message
         
         self.mobile_base_vel_publisher.publish(control_msg)
         
     def run(self):
         
-        rospy.Subscriber("/locobot/mobile_base/odom", Odometry, self.mobile_base_callback)
-        
+        rospy.Subscriber("/locobot/mobile_base/odom", Odometry, self.locobot_odom_callback)
+        if self.robot_type == "physical":
+            rospy.Subscriber("/vrpn_client_node/" + self.robot_name + "/pose", PoseStamped, self.mocap_callback)
+
         self.t_init = rospy.Time.now()
 
         rate = rospy.Rate(CTRL_HZ)
@@ -192,6 +291,7 @@ class LocobotFollowTrajectory(object):
                 counter = 0
             if counter == 0:
                 self.path_visual.publish(self.path_msg)
+                self.traj_visual.publish(self.traj_msg)
             counter += 1
 
             rate.sleep()
--- me326_locobot_group5/scripts/trajectory_generator.py
+++ me326_locobot_group5/scripts/trajectory_generator.py
@@ -43,12 +43,11 @@ def compute_arc_length(V, t):
             t[i]. This has length T.
     """
 
-    s = None
     s = cumtrapz(V, t, initial=0)
 
     return s
 
-def rescale_V(V, om, V_max, om_max):
+def rescale(s, V, om, V_max, a_max, om_max):
     """
     This function computes V_tilde, given the unconstrained solution V, and om.
     Inputs:
@@ -60,38 +59,42 @@ def rescale_V(V, om, V_max, om_max):
         V_tilde: Rescaled velocity that satisfies the control constraints.
     """
 
-    V_tilde = np.sign(V)/np.max(np.abs([1/V, np.ones(np.shape(V))/V_max, 1/V*om/om_max]), axis=0)
+    # velocity to satisfy both V_max and om_max
+    V_tilde = np.sign(V)/np.max(np.abs([np.ones(np.shape(V))/V_max, 1/V*om/om_max]), axis=0)
 
-    return V_tilde
+    # forwards pass
+    V_tilde[0] = 0
+    for i in range(len(V_tilde)-1):
+        Vi = V_tilde[i]
+        ds = s[i+1]-s[i]
+        Vf_max = np.sqrt(Vi**2 + 2*a_max*ds)
+        V_tilde[i+1] = min(Vf_max, V_tilde[i+1])
+    
+    # backwards pass
+    V_tilde[-1] = 0
+    for i in range(len(V_tilde)-1, 0, -1):
+        Vf = V_tilde[i]
+        ds = s[i]-s[i-1]
+        Vi_max = np.sqrt(Vf**2 + 2*a_max*ds)
+        V_tilde[i-1] = min(Vi_max, V_tilde[i-1])
 
-def compute_tau(V_tilde, s):
-    """
-    This function computes the new time history tau as a function of s.
-    Inputs:
-        V_tilde: a sequence of scaled velocities of length T.
-        s: a sequence of arc-length of length T.
-    Output:
-        tau: the new time history for the sequence. tau[i] is the time at s[i]. This has length T.
-    """
-
-    tau = cumtrapz(1/V_tilde, s, initial=0)
-
-    return tau
-
-def rescale_om(V, om, V_tilde):
-    """
-    This function computes the rescaled om control.
-    Inputs:
-        V: vector of velocities of length T. Solution from the unconstrained, differential flatness problem.
-        om:  vector of angular velocities of length T. Solution from the unconstrained, differential flatness problem.
-        V_tilde: vector of scaled velocities of length T.
-    Output:
-        om_tilde: vector of scaled angular velocities
-    """
-
+    # compute angular velocity
     om_tilde = om*V_tilde/V
 
-    return om_tilde
+    # compute timestamps
+    tau = np.zeros_like(V_tilde)
+    for i in range(1, len(tau)):
+        ds = s[i]-s[i-1]
+        Vi, Vf = V_tilde[i-1], V_tilde[i]
+        accel = (Vf**2 - Vi**2)/(2*ds)
+        if abs(accel) > 1e-6:
+            tau[i] = tau[i-1] + (Vf-Vi)/accel
+        elif abs(Vi) > 1e-6:
+            tau[i] = tau[i-1] + ds/Vi
+        else:
+            raise ValueError('zero velocity at i = ' + i)
+
+    return V_tilde, om_tilde, tau
 
 def interpolate_traj(traj, tau, V_tilde, om_tilde, dt, s_f):
     """
@@ -172,7 +175,7 @@ def compute_smoothed_traj(path, V_des, alpha, dt):
 
     return traj_smoothed, t_smoothed
 
-def modify_traj_with_limits(traj, t, V_max, om_max, dt):
+def modify_traj_with_limits(traj, t, V_max, a_max, om_max, dt):
     """
     Modifies an existing trajectory to satisfy control limits and
     interpolates for desired timestep.
@@ -191,9 +194,7 @@ def modify_traj_with_limits(traj, t, V_max, om_max, dt):
 
     V,om = compute_controls(traj=traj)
     s = compute_arc_length(V, t)
-    V_tilde = rescale_V(V, om, V_max, om_max)
-    tau = compute_tau(V_tilde, s)
-    om_tilde = rescale_om(V, om, V_tilde)
+    V_tilde, om_tilde, tau = rescale(s, V, om, V_max, a_max, om_max)
     s_f = State(traj[-1, 0], traj[-1, 1], V_tilde[-1], traj[-1, 2])
 
     t_new, V_scaled, om_scaled, traj_scaled = interpolate_traj(traj, tau, V_tilde, om_tilde, dt, s_f)
